3e60d0cd51767f1934b9701525f91418e1993683
Ryan Long <cryanlong38@gmail.com>
2020-04-22 23:17:22 -0500

    Messsed around with nullable workaround, coulndt solve

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,57 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// The movies to display on the index page 
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
-        public void OnGet()
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+         [BindProperty]
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// Gets and sets the IMDB minimium rating
+        /// </summary>
+        
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// Gets and sets the IMDB maximum rating
+        /// </summary>
+        
+        public double? IMDBMax { get; set; }
+
+
+        /// <summary>
+        /// Gets the search results for display on the page
+        /// </summary>
+        public void OnGet(double? IMDBMin, double? IMDBMax)
         {
-
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            //SearchTerms = Request.Query["SearchTerms"];
+            //MPAARatings = Request.Query["MPAARatings"];
+            //Genres = Request.Query["Genres"];
+            //IMDBMin = float.Parse(Request.Query["IMDBMin"]);
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
     }
 }
